f56a3be4b51d6d888307613dd0745bb36b83a51b
nightfury0245 <dheeraj9kaushik@gmail.com>
2024-05-04 11:04:08 +0530

    organised tiles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,28 @@
 // src/App.js
-import React from 'react';
+import React from "react";
 import "./styles/dashboard.css";
 function App() {
-    return (
-        <div id="dashboard-box">
-            <button>
-            <div class="new-order item">
-                <h1>New Order</h1>
-            </div>
-            </button>
-            <button>
-            <div class="track-order item">
-                <h1>Track Order</h1>
-            </div>
-            </button>
-            <button>
-            <div class="inventory item">
-                <h1>Inventory</h1>
-            </div>
-            </button>
-            <button>
-            <div class="generate-label item">
-                <h1>Generate Label</h1>
-            </div>
-            </button>            
+  return (
+    <div className="root">
+      <div id="dashboard-box">
+        <div class="new-order item">
+          <h1>New Order</h1>
         </div>
-    );
+
+        <div class="track-order item">
+          <h1>Track Order</h1>
+        </div>
+
+        <div class="inventory item">
+          <h1>Inventory</h1>
+        </div>
+
+        <div class="generate-label item">
+          <h1>Generate Label</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
