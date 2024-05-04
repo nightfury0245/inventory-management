3d534aa38434ab44a73f8b2100ad24e6861ec882
nightfury0245 <dheeraj9kaushik@gmail.com>
2024-05-05 00:18:28 +0530

    Dashboard done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,35 @@
-// src/App.js
-import React from "react";
+import React, { useState } from "react";
 import "./styles/dashboard.css";
+import NewOrder from "./NewOrder/NewOrder.js";
+import TrackOrders from "./TrackOrders/TrackOrders.js";
+import Inventory from "./Inventory/Inventory.js";
+import GenerateLabel from "./GenerateLabel/GenerateLabel.js";
+
 function App() {
+  const [state, setState] = useState({
+    newOrder: false,
+    trackOrder: false,
+    inventory: false,
+    generateLabel: false
+  });
+
+  const handleItemClick = (key) => {
+    setState(prevState => ({ ...prevState, [key]: true }));
+  };
+
   return (
-    <div className="root">
-      <div id="dashboard-box">
-        <div class="new-order item">
-          <h1>New Order</h1>
-        </div>
-
-        <div class="track-order item">
-          <h1>Track Order</h1>
-        </div>
-
-        <div class="inventory item">
-          <h1>Inventory</h1>
-        </div>
-
-        <div class="generate-label item">
-          <h1>Generate Label</h1>
-        </div>
+    <>
+      <div className="container">
+        <div className="item new-order" onClick={() => handleItemClick("newOrder")}>New Order</div>
+        <div className="item track-orders" onClick={() => handleItemClick("trackOrder")}>Track orders</div>
+        <div className="item inventory" onClick={() => handleItemClick("inventory")}>Inventory</div>
+        <div className="item generate-label" onClick={() => handleItemClick("generateLabel")}>Generate label</div>
       </div>
-    </div>
+      {state.newOrder && window.location.assign("./NewOrder/NewOrder.js")}
+      {state.trackOrder && <TrackOrders />}
+      {state.inventory && <Inventory />}
+      {state.generateLabel && <GenerateLabel />}
+    </>
   );
 }
 
---
+++ frontend/src/GenerateLabel/GenerateLabel.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GenerateLabel() {
+  return (
+    <div>GenerateLabel</div>
+  )
+}
+
+export default GenerateLabel
