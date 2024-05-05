1b4fca63b0e6cef8d30d63257b39f5e90f72dc5d
nightfury0245 <dheeraj9kaushik@gmail.com>
2024-05-05 13:55:43 +0530

    finished new-order frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,22 +13,58 @@ function App() {
     generateLabel: false
   });
 
+  const [showContainer, setShowContainer] = useState(true);
+
   const handleItemClick = (key) => {
     setState(prevState => ({ ...prevState, [key]: true }));
+    setShowContainer(false);
+  };
+
+  const handleBackClick = () => {
+    setState({
+      newOrder: false,
+      trackOrder: false,
+      inventory: false,
+      generateLabel: false
+    });
+    setShowContainer(true);
   };
 
   return (
     <>
-      <div className="container">
-        <div className="item new-order" onClick={() => handleItemClick("newOrder")}>New Order</div>
-        <div className="item track-orders" onClick={() => handleItemClick("trackOrder")}>Track orders</div>
-        <div className="item inventory" onClick={() => handleItemClick("inventory")}>Inventory</div>
-        <div className="item generate-label" onClick={() => handleItemClick("generateLabel")}>Generate label</div>
-      </div>
-      {state.newOrder && window.location.assign("./NewOrder/NewOrder.js")}
-      {state.trackOrder && <TrackOrders />}
-      {state.inventory && <Inventory />}
-      {state.generateLabel && <GenerateLabel />}
+      {showContainer && (
+        <div className="container">
+          <div className="item new-order" onClick={() => handleItemClick("newOrder")}>New Order</div>
+          <div className="item track-orders" onClick={() => handleItemClick("trackOrder")}>Track orders</div>
+          <div className="item inventory" onClick={() => handleItemClick("inventory")}>Inventory</div>
+          <div className="item generate-label" onClick={() => handleItemClick("generateLabel")}>Generate label</div>
+        </div>
+      )}
+      {state.newOrder && (<>
+        <button className="back-button" onClick={handleBackClick}>Back</button>
+        <div className="NewOrderContainer">
+          <NewOrder />
+        </div>
+        </>
+      )}
+      {state.trackOrder && (
+        <div>
+          <button className="back-button" onClick={handleBackClick}>Back</button>
+          <TrackOrders />
+        </div>
+      )}
+      {state.inventory && (
+        <div>
+          <button className="back-button" onClick={handleBackClick}>Back</button>
+          <Inventory />
+        </div>
+      )}
+      {state.generateLabel && (
+        <div>
+          <button className="back-button" onClick={handleBackClick}>Back</button>
+          <GenerateLabel />
+        </div>
+      )}
     </>
   );
 }
